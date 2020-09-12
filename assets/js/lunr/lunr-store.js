e0fcc548d33b68af3cd397fef03a29c6935772b7
notabhijeet <abhisri003@gmail.com>
2020-09-12 19:19:49 +0530

    Updated theme to minimal mistakes and added a new post

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,19 @@
+var store = [{
+        "title": "Is this another blog?",
+        "excerpt":"Is this another blog? object Hello { println(\"Hello World!\") } Having written a lot of hello world programs I want to share my experience with other developers. I could use feedback from peers. I am going to document code snippets, concepts, tech news, interview tips, and a few random stuff....","categories": ["about"],
+        "tags": ["welcome","blog"],
+        "url": "http://localhost:4000/about/is-this-another-blog/",
+        "teaser": null
+      },{
+        "title": "How to remove null from code?",
+        "excerpt":"Introduction Scala: Though the concepts mentioned in this post are related to Scala, they are universal. Thought: A program should not use null. Null, if not handled, can be troublesome just like an unhandled exception. One should actively try and avoid its usage. Here are a couple of ways to...","categories": ["programming"],
+        "tags": ["programming"],
+        "url": "http://localhost:4000/programming/the-one-with-null/",
+        "teaser": null
+      },{
+        "title": "Programmers best friend IDE",
+        "excerpt":"A programmer needs a good IDE A good IDE is a programmers best friend. Writing clean code requires one to spend time in their code base and to have a look and feel of various parts of code. Search files, syntax highlighting, completions, renaming, formatting with fmt flavored styles etc...","categories": ["programming"],
+        "tags": ["programming"],
+        "url": "http://localhost:4000/programming/programmers-best-friend/",
+        "teaser": null
+      }]
