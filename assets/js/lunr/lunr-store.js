a1e3b8d1f811154bc20ead1b9bf5e85597707b92
notabhijeet <abhisri003@gmail.com>
2021-01-17 18:52:18 +0530

    removed jekyll blog

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,6 +1,6 @@
 var store = [{
-        "title": "Is this another blog?",
-        "excerpt":"Is this another blog? object Hello { println(\"Hello World!\") } Having written a lot of hello world programs I want to share my experience with other developers. I could use feedback from peers. I am going to document code snippets, concepts, tech news, interview tips, and a few random stuff....","categories": ["about"],
+        "title": "Is This Another Blog",
+        "excerpt":"Is this another blog? object Hello { println(&quot;Hello World!&quot;) } Having written a lot of hello world programs I want to share my experience with other developers. I could use feedback from peers. I am going to document code snippets, concepts, tech news, interview tips lifestyle, and a few random...","categories": ["about"],
         "tags": ["welcome","blog"],
         "url": "http://localhost:4000/about/is-this-another-blog/",
         "teaser": null
@@ -15,5 +15,17 @@ var store = [{
         "excerpt":"A programmer needs a good IDE A good IDE is a programmers best friend. Writing clean code requires one to spend time in their code base and to have a look and feel of various parts of code. Search files, syntax highlighting, completions, renaming, formatting with fmt flavored styles etc...","categories": ["programming"],
         "tags": ["programming"],
         "url": "http://localhost:4000/programming/programmers-best-friend/",
+        "teaser": null
+      },{
+        "title": "Scala upgrade to 2.13",
+        "excerpt":" Whats next   upgradation   ","categories": ["posts"],
+        "tags": ["scala","programming"],
+        "url": "http://localhost:4000/posts/scala-upgrade/",
+        "teaser": null
+      },{
+        "title": "running plan during pandemic, 2020",
+        "excerpt":" Running  Running has been a part of my life since my bachelor&#8217;s and I have found one   way or other to keep at it.  My current schedule  What I would like to achieve in 2021  What I would recommend for beginners  Conclusion  This   ","categories": ["about"],
+        "tags": ["running","lifestyle"],
+        "url": "http://localhost:4000/about/running-lifestyle-during-pandemic-2020/",
         "teaser": null
       }]
